9650f6f7250c0436808620824eda9aa90188cb36
Ozer Altun <ozer.altun93@gmail.com>
2021-01-19 04:35:59 +0300

    Rocket at its launchpad

---
+++ Assets/Scenes/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)){
+            print("Thrusting...");
+        }
+        
+        //We should be able to thrust and rotate at the same time, therefore different if
+        if(Input.GetKey(KeyCode.A)){
+            print("Rotating Left...");
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            print("Rotating Right...");
+        }
+        else{
+            //Nothing
+        }
+
+    }
+}
