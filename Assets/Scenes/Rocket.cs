9d22e41504ff0fa261082947840427004a405bba
Ozer Altun <ozer.altun93@gmail.com>
2021-01-19 04:48:35 +0300

    Giving some thrust

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    Rigidbody rigidBody;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -19,7 +24,7 @@ public class Rocket : MonoBehaviour
     private void ProcessInput()
     {
         if(Input.GetKey(KeyCode.Space)){
-            print("Thrusting...");
+            rigidBody.AddRelativeForce(Vector3.up);
         }
         
         //We should be able to thrust and rotate at the same time, therefore different if
