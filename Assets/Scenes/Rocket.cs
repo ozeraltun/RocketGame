5ab8830e073ad107266cda5396d7daf5c332b493
ozeraltun <ozer.altun93@gmail.com>
2021-01-28 22:00:01 +0300

    Rotations and Audio are added

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -1,18 +1,18 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
-
+    AudioSource audioSource;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -25,14 +25,20 @@ public class Rocket : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space)){
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying){
+                audioSource.Play();
+            }
+        }
+        else{
+            audioSource.Stop();
         }
         
         //We should be able to thrust and rotate at the same time, therefore different if
         if(Input.GetKey(KeyCode.A)){
-            print("Rotating Left...");
+            transform.Rotate(Vector3.forward);
         }
         else if(Input.GetKey(KeyCode.D)){
-            print("Rotating Right...");
+            transform.Rotate(-Vector3.forward);
         }
         else{
             //Nothing
